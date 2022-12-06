7e8f8a40e1698ca0c79b878d907d05f0ad1849f3
Mariagoretti Kimani <mgmkimani@gmail.com>
2022-12-06 07:45:51 +0300

    js file

--- src/index.js
+++ src/index.js
@@ -32,3 +32,24 @@ function getAnimals() {
     const characterVotes = document.querySelector("span#vote-count");
     characterVotes.innerHTML = character.votes;
   }
+
+  //adding votes
+  const inputVotes = document.querySelector("input#votes");
+  const animalVotes = document.querySelector("span#vote-count");
+  const form = document.querySelector("form#votes-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(currentAnimal);
+    currentAnimal.votes += parseInt(e.target.votes.value, 10);
+    showAnimal(currentAnimal);
+    form.reset();
+  });
+
+  //  the reset button
+  const resetVotes = document.querySelector("button#reset-btn");
+  resetVotes.style.cursor = "pointer";
+  resetVotes.addEventListener("click", () => {
+    currentAnimal.votes = 0;
+    showAnimal(currentAnimal);
+    form.reset();
+  });
